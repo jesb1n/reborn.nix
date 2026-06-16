cc5361eab2b1d156271496bddfb8bbc650188e5e
Jesbin Joseph <jesbin.joseph@egovernments.org>
2026-06-16 16:21:05 +0530

    local backend

--- backend.tf
+++ backend.tf
@@ -1,16 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket                      = "my-tofu-backend"
-    region                      = "us-ashburn-1"
-    key                         = "infra/tf.tfstate"
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    use_path_style              = true
-    skip_s3_checksum            = true
-    skip_metadata_api_check     = true
-    endpoints = {
-      s3 = "https://<your-namespace>.compat.objectstorage.<your-region>.oraclecloud.com"
-    }
-  }
+  backend "local" {}
 }
