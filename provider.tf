77b917b7a64bf2f3a529421e3558169307d3ce6e
Jesbin Joseph <jesbin.joseph@egovernments.org>
2026-06-16 16:21:29 +0530

    oc cli auth reuse

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "oci" {
-  tenancy_ocid = var.tenancy_ocid
-  user_ocid    = var.user_ocid
-  fingerprint  = var.fingerprint
-  private_key  = var.OCA_PRIVATE_KEY
-  region       = var.region
+  auth                = "SecurityToken"
+  config_file_profile = "DEFAULT"
+  region              = var.region
 }
