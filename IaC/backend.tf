4a41a3275b0faf302e3dad35be08d19052c0fa67
jesb1n <joseph@jesb.in>
2026-07-04 00:59:48 +0530

    Add S3 backend for Terraform via Garage NodePort

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -1,3 +1,12 @@
 terraform {
-  backend "local" {}
+  backend "s3" {
+    bucket                      = "tofu-backend"
+    key                         = "terraform.tfstate"
+    region                      = "garage"
+    endpoint                    = "http://100.69.231.117:31900"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style              = true
+  }
 }
