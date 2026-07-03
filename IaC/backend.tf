42d6540682875e2dd67afb135b895c105707abeb
jesb1n <joseph@jesb.in>
2026-07-04 01:04:53 +0530

    fix: update S3 backend key for Terraform state file

--- IaC/backend.tf
+++ IaC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                      = "tofu-backend"
-    key                         = "terraform.tfstate"
+    key                         = "beijns/terraform.tfstate"
     region                      = "garage"
     endpoint                    = "http://100.69.231.117:31900"
     skip_credentials_validation = true
