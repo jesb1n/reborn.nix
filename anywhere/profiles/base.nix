298b20980483a3dd6b39ba264c875a05b5384b62
jesbinjoseph <jesbin.joseph@egovernments.org>
2026-07-27 21:12:59 +0530

    base: disable PAM for sudo (broken in new nixpkgs-unstable), use proper password hash

--- anywhere/profiles/base.nix
+++ anywhere/profiles/base.nix
@@ -65,4 +65,7 @@
   };
 
   security.sudo.wheelNeedsPassword = false;
+
+  # sudo PAM integration is broken in this nixpkgs-unstable — disable it
+  environment.etc."sudo.conf".text = "SetOpt disable_pam 1\n";
 }
